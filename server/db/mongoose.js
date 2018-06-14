c0cb468f2f78c92a2bda72607ecd1ced11ddf84f
smithshelke <smithshelke@gmail.com>
2018-06-14 22:06:10 +0900

    Add DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ var client = {
   local:'mongodb://localhost:27017/TodoApp',
   heroku:'mongodb://smithshelke:smith11mr@ds159400.mlab.com:59400/mydb'
 }
-mongoose.connect(client.heroku||client.local);
+mongoose.connect(client.local);
 module.exports = {
   mongoose
 };
